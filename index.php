c3ccbd1714117fdbe9de41d59d46b2c53282b677
stuart-antcliff <102359353+stuart-antcliff@users.noreply.github.com>
2025-06-06 08:33:12 +0100

    Update index.php
    To display Get Arguments

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Get the query string from the URL
+    $query_string = $_SERVER['QUERY_STRING'];
 
-echo "Hello World!  SJA Woz Here";
+    // Parse the query string into an associative array
+    parse_str($query_string, $query_params);
+
+    // Display the query parameters
+    echo '<h2>Query Parameters:</h2>';
+    echo '<ul>';
+    foreach ($query_params as $key => $value) {
+        echo '<li>' . htmlspecialchars($key) . ': ' . htmlspecialchars($value) . '</li>';
+    }
+    echo '</ul>';
+} else {
+    echo 'Invalid request method.';
+}
+?>
